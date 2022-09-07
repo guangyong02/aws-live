2d1d7967368faef86d60247a08605058e871dae9
guangyong02 <67313893+guangyong02@users.noreply.github.com>
2022-09-07 17:06:34 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ccved0rmz6ie.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tehguangyong-employee"
+customregion = "us-east-1"
